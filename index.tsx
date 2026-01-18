8253252f8612e86169c5eaebb73cb3765411fc5d
berdinskikhdaniil-cmd <berdinskikh.daniil@gmail.com>
2026-01-18 09:50:40 +0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // Если есть стили, если нет - удалите эту строку
 
+// Находим элемент root в HTML
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (rootElement) {
+  // Создаем корень приложения (новый стандарт React 18)
+  const root = ReactDOM.createRoot(rootElement);
+  
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("НЕ НАЙДЕН элемент с id='root' в index.html");
+}
