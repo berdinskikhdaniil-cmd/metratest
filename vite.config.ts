e64e014dcd4b68671a0045923e772236b2b68619
berdinskikhdaniil-cmd <berdinskikh.daniil@gmail.com>
2026-01-18 10:03:46 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, (process as any).cwd(), '');
-  
-  return {
-    plugins: [react()],
-    base: './',
-    optimizeDeps: {
-      include: ['@google/genai']
-    },
-    build: {
-      commonjsOptions: {
-        include: [/@google\/genai/, /node_modules/]
-      }
-    },
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
-      'process.env': {}
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
